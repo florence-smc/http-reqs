6423d3b0c6dc19875206461ca01f5f63f67adc31
Florence <smc.florence@gmail.com>
2017-02-13 17:45:53 +0000

    moved the options object outside of the function for more modularity

---
+++ app-3.js
@@ -0,0 +1,29 @@
+// hardcoding values reduces modularity
+
+var https = require('https');
+var chunk = '';
+
+	var options = {
+		host: 'sytantris.github.io',
+		path: '/http-examples/step3.html'
+	};
+
+function getAndPrintHTML () {
+
+	/* Add your code here */
+
+	https.get(options, function(response) {
+		response.setEncoding('utf8');
+
+		response.on('data', function(data) {
+			chunk += data;
+		});
+
+		response.on('end', function() {
+			console.log('Data Received: ', chunk, '\n');
+			console.log('Response stream complete.');
+		});
+	})
+}
+
+getAndPrintHTMLChunks(options, options);
